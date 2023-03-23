ee3a65be61b0703ac28974a229387a8fa9267411
teang sreyroth <teangsreyrotha508@gmail.com>
2023-03-23 16:43:56 +0700

    Update Hello.java

--- Exercise/Hello.java
+++ Exercise/Hello.java
@@ -3,5 +3,6 @@ package Exercise;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("I'm Mouylang");
+        System.out.println("I'm Sreyroth");
     }
 }
