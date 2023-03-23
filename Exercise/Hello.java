acb81e6cf51f8754429ee797ae9738c2646dbb38
SAMSOKREAKSMEY <127179736+SAMSOKREAKSMEY@users.noreply.github.com>
2023-03-23 18:28:41 +0700

    Update Hello.java

--- Exercise/Hello.java
+++ Exercise/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("I'm Mouylang");
         System.out.println("I'm Sreyroth");
+        System.out.println("Ï'm reaksmey");
     }
 }
